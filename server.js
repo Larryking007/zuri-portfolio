2b31521c57cfea02c3abd7170dacd55172976aa3
Larryking007 <ajikobilanre007@gmail.com>
2022-06-16 17:11:53 +0100

    base url

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var http = require("http");
 var fs = require("fs");
-const { error } = require("console");
+var url = require("url");
+
 http
   .createServer(function (req, res) {
-    fs.readFile("home.html", function (err, data) {
+    var q = url.parse(req.url, true);
+    var filename;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       res.end();
+
+      if (url === "/home") {
+        filename = "home.html";
+      } else if (url === "/about") {
+        filename = "about.html";
+      } else if (url === "/contact") {
+        filename = "contact.html";
+      } else {
+        console.log("Hello world");
+      }
     });
   })
   .listen(8080);
